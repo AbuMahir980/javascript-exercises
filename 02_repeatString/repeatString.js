d6da55a4341466f9eb893ffbdc10d890149893a5
Qudus Lawal <lawalqudus980@gmail.com>
2023-11-02 11:14:26 +0100

    Complete second exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) return "ERROR";
+    let repeatStringVal = ""
+    for(i = 0; i < num; i++){
+        repeatStringVal += string;
+    
+    }
+    return repeatStringVal
 };
+console.log(repeatString('hey',-1))
 
 // Do not edit below this line
 module.exports = repeatString;
