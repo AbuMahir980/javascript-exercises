c6a6a6d82e79422915d8f8d826b300083e562c10
Qudus Lawal <lawalqudus980@gmail.com>
2023-11-02 11:31:19 +0100

    Complete third exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(stringVal) {
+    stringVal = stringVal.split('').reverse().join('')
+    return stringVal
 };
-
+console.log(reverseString(' '))
 // Do not edit below this line
 module.exports = reverseString;
