a57a6cf5b80d6a8f394d418d8c9df8916a68b3af
Qudus Lawal <lawalqudus980@gmail.com>
2023-11-05 21:39:37 +0100

    Update changes

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,6 @@
 const removeFromArray = function(arr, ...args) {
-    return arr.filter(v => !args.includes(v)); 
+    return arr.filter(item => !args.includes(item)); 
 };
-
+console.log(removeFromArray([1, 2, 3, 4], 3));
 // Do not edit below this line
 module.exports = removeFromArray;
