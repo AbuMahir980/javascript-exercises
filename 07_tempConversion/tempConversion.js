05fe430b4449dfd16b3a1e40371c524f6bba90f7
Qudus Lawal <lawalqudus980@gmail.com>
2023-11-06 11:28:36 +0100

    Solve tempConversion challenge

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitVal) {
+  let toCelsius = (fahrenheitVal - 32) * 5/ 9;
+	return parseFloat(toCelsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsiusVal) {
+	let toFahrenheit = (celsiusVal * 1.8) + 32;
+	return parseFloat(toFahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
